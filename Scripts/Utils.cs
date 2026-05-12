ff3fd2876c368e56faf451b779776ffb1b7ca58c
chromy0430 <chromy0430@gmail.com>
2026-05-12 18:04:31 +0900

    Update Utils.cs
    Awaitable 대기 함수 추가, 배열 간 데이터 복사 함수 추가, Remap 함수 추가

--- Scripts/Utils.cs
+++ Scripts/Utils.cs
@@ -1,8 +1,97 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
+
+using System;
+using System.Threading.Tasks;
 
 public static class Utils 
 {
+    [Tooltip("유니티 Awaitable용 대기 함수")]
+    public static Awaitable WaitUntil(this Func<bool> condition, int pollIntervalMs = 33) 
+    {
+        if (condition == null) throw new ArgumentNullException(nameof(condition));
+        if (pollIntervalMs <= 0) throw new ArgumentOutOfRangeException(nameof(pollIntervalMs));
+
+        var source = new AwaitableCompletionSource();
+
+        if (condition())
+        {
+            source.SetResult();
+            return source.Awaitable;
+        }
+        
+        var interval = TimeSpan.FromMilliseconds(pollIntervalMs);
+
+        async void Poll()
+        {
+            while (!condition())
+            {
+                await Awaitable.WaitForSecondsAsync((float)interval.TotalSeconds);
+            }
+            source.SetResult();
+        }
+
+        Poll();
+        return source.Awaitable;
+    }
+    
+    // Tasks
+    // Default poll interval of 33ms = one frame at 30 FPS (1000 ms / 30 = 33.33ms)
+    [Tooltip("C# Task용 대기 + 타임아웃")]
+    public static async Task<bool> WaitUntil(this Func<bool> condition, int timeoutMs = -1, int pollIntervalMs = 33)
+    {
+        if (condition == null) throw new ArgumentNullException(nameof(condition));
+        if (pollIntervalMs <= 0) throw new ArgumentOutOfRangeException(nameof(pollIntervalMs));
+
+        var waitTask = RunWaitLoop(condition, pollIntervalMs);
+
+        if (timeoutMs < 0)
+        {
+            await waitTask;
+            return true;
+        }
+        
+        var timeoutTask = Task.Delay(timeoutMs);
+        var finished = await Task.WhenAny(waitTask, timeoutTask);
+        return finished == waitTask;
+    }
+
+    static async Task RunWaitLoop(Func<bool> condition, int pollIntervalMs)
+    {
+        while (!condition())
+        {
+            await Task.Delay(pollIntervalMs).ConfigureAwait(false);
+        }
+    }
+    
+    // Copy
+    public static void BlockCopy<T>(this Span<T> src, int srcOffset, Span<T> dst, int dstOffset, int count)
+    {
+        if (((uint)(srcOffset + count)) > (uint)src.Length) throw new ArgumentException("Source span is too small");
+        if (((uint)(dstOffset + count)) > (uint)dst.Length) throw new ArgumentException("Destination span is too small"); 
+        
+        src.Slice(srcOffset, count).CopyTo(dst.Slice(dstOffset));
+    }
+    
+    [Tooltip("배열 간 데이터 복사를 위한 헬퍼 함수")]
+    public static void BlockCopy<T>(this T[] src, int srcOffset, T[] dst, int dstOffset, int count)
+    {
+        if (src == null) throw new ArgumentNullException(nameof(src));
+        if (dst == null) throw new ArgumentNullException(nameof(dst));  
+        
+        src.AsSpan().BlockCopy(srcOffset, dst.AsSpan(), dstOffset, count);
+        //Array.Copy(src, srcOffset, dst, dstOffset, count);
+    }
+    
+    // Math
+    [Tooltip("플레이어 입력 값(0~1)을 이동속도(10~100)로 변환 하거나, 체력 바의 백분율을 UI바의 길이로 변환하는 경우")]
+    public static float Remap(this float value, float from1, float to1, float from2, float to2)
+    {
+        return Mathf.Lerp(from2, to2, Mathf.InverseLerp(from1, to1, value));
+    }
+    
     // Vector3 Extension
     [Tooltip("EX) transform.position.With(y : number)")]
     public static Vector3 With(this Vector3 vector, float? x = null, float? y = null, float? z = null)
